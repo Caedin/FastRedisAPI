8c1f00eb21a4979146527f24f57807d490fc6c12
Keith <keith.dyer@engie.com>
2022-01-21 15:43:15 -0600

    compressed code

--- main.py
+++ main.py
@@ -10,62 +10,37 @@ app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_credentials=True, 
 
 @app.get("/{key}/json")
 async def get_json(key, path: Optional[str] = "."):
-    print(f"Request received for GET {key}/json")
-    dbval = redis_helper.get_json(key, path)
-    if dbval == "":
-        print("No value found in db")
-        return ""
-    print("success")
-    return dbval
+    return redis_helper.get_json(key, path)
 
 @app.post("/{key}/json")
 async def post_json(key, request: Request, path: Optional[str] = "."):
-    print(f"Request received for SET {key}/json")
-    val = await request.json()
-    redis_helper.set_json(key, path, val)
-    print("success")
+    redis_helper.set_json(key, path, await request.json())
 
 @app.delete("/{key}/json")
 async def delete_json(key):
-    print(f"Request received for DELETE {key}/json")
     redis_helper.delete(key)
-    print("success")
 
 @app.get("/{key}/pop")
 async def pop(key, path: Optional[str] = ".", index: Optional[int] = -1):
-    print(f"Request received for POP {key}/json")
     redis_helper.pop_json_array(key, path, index)
-    print("success")
 
 @app.post("/{key}/insert")
 async def insert(key, request: Request, path: Optional[str] = ".", index: Optional[int] = 0):
-    print(f"Request received for INSERT {key}/json")
     val = await request.body()
     redis_helper.insert_json_array(key, path, index, val.decode("utf8"))
-    print("success")
 
 @app.get("/{key}", response_model=str)
 async def get(key: str):
-    print(f"Request received for GET {key}")
-    dbval = redis_helper.get(key)
-    if dbval == "":
-        print("No value found in db")
-        return ""
-    print("success")
-    return dbval
+    return redis_helper.get(key)
 
 @app.post("/{key}")
 async def post(key, request: Request, ttl: Optional[int] = 0):
     val = await request.body()
-    print(f"Request received for SET {key} with value {str(val)}")
-    redis_helper.set(key, str(val), ttl)
-    print("success")
+    redis_helper.set(key, val.decode("utf8"), ttl)
 
 @app.delete("/{key}")
 async def delete(key):
-    print(f"Request received for DELETE {key}")
     redis_helper.delete(key)
-    print("success")
 
 if __name__ == '__main__':
     uvicorn.run(
